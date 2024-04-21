3135817ec668ff39a9cb2aeb09e49b1191153542
Pawel Powroznik <ppowroznik@gmail.com>
2024-04-21 14:13:51 +0200

    Initial Commit

---
+++ MyFirstApp/MyFirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstAppApp.swift
+//  MyFirstApp
+//
+//  Created by Paweł Powroźnik on 21/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
